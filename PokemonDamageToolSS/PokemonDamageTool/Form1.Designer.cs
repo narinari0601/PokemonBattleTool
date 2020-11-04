8bf7f1882dbabed01b77fb60d352cf490ba450b0
Narinari0601 <kouta19960601.kizuna-reo.t-mk@ezweb.ne.jp>
2020-11-04 23:57:45 +0900

    プロジェクト準備 軽い計算はできるように

---
+++ PokemonDamageToolSS/PokemonDamageTool/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace PokemonDamageTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.attackLabel = new System.Windows.Forms.Label();
+            this.defenseLabel = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.powerLabel = new System.Windows.Forms.Label();
+            this.damageLabel = new System.Windows.Forms.Label();
+            this.sameTypeCB = new System.Windows.Forms.CheckBox();
+            this.supperEffectiveCB = new System.Windows.Forms.CheckBox();
+            this.notVeryEffectiveCB = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(87, 144);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(105, 22);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(620, 441);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "計算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(488, 144);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 3;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // attackLabel
+            // 
+            this.attackLabel.AutoSize = true;
+            this.attackLabel.Location = new System.Drawing.Point(113, 67);
+            this.attackLabel.Name = "attackLabel";
+            this.attackLabel.Size = new System.Drawing.Size(52, 15);
+            this.attackLabel.TabIndex = 4;
+            this.attackLabel.Text = "攻撃側\r\n";
+            // 
+            // defenseLabel
+            // 
+            this.defenseLabel.AutoSize = true;
+            this.defenseLabel.Location = new System.Drawing.Point(520, 67);
+            this.defenseLabel.Name = "defenseLabel";
+            this.defenseLabel.Size = new System.Drawing.Size(52, 15);
+            this.defenseLabel.TabIndex = 5;
+            this.defenseLabel.Text = "防御側";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(266, 144);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            250,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown3.TabIndex = 6;
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // powerLabel
+            // 
+            this.powerLabel.AutoSize = true;
+            this.powerLabel.Location = new System.Drawing.Point(302, 67);
+            this.powerLabel.Name = "powerLabel";
+            this.powerLabel.Size = new System.Drawing.Size(52, 15);
+            this.powerLabel.TabIndex = 7;
+            this.powerLabel.Text = "技威力";
+            // 
+            // damageLabel
+            // 
+            this.damageLabel.Location = new System.Drawing.Point(720, 146);
+            this.damageLabel.Name = "damageLabel";
+            this.damageLabel.Size = new System.Drawing.Size(113, 33);
+            this.damageLabel.TabIndex = 8;
+            this.damageLabel.Text = "ダメージ";
+            this.damageLabel.Click += new System.EventHandler(this.damageLabel_Click);
+            // 
+            // sameTypeCB
+            // 
+            this.sameTypeCB.AutoSize = true;
+            this.sameTypeCB.Location = new System.Drawing.Point(128, 260);
+            this.sameTypeCB.Name = "sameTypeCB";
+            this.sameTypeCB.Size = new System.Drawing.Size(91, 19);
+            this.sameTypeCB.TabIndex = 12;
+            this.sameTypeCB.Text = "タイプ一致";
+            this.sameTypeCB.UseVisualStyleBackColor = true;
+            this.sameTypeCB.CheckedChanged += new System.EventHandler(this.sameTypeCB_CheckedChanged);
+            // 
+            // supperEffectiveCB
+            // 
+            this.supperEffectiveCB.AutoSize = true;
+            this.supperEffectiveCB.Location = new System.Drawing.Point(305, 260);
+            this.supperEffectiveCB.Name = "supperEffectiveCB";
+            this.supperEffectiveCB.Size = new System.Drawing.Size(52, 19);
+            this.supperEffectiveCB.TabIndex = 13;
+            this.supperEffectiveCB.Text = "2倍";
+            this.supperEffectiveCB.UseVisualStyleBackColor = true;
+            this.supperEffectiveCB.CheckedChanged += new System.EventHandler(this.supperEffectiveCB_CheckedChanged);
+            // 
+            // notVeryEffectiveCB
+            // 
+            this.notVeryEffectiveCB.AutoSize = true;
+            this.notVeryEffectiveCB.Location = new System.Drawing.Point(499, 260);
+            this.notVeryEffectiveCB.Name = "notVeryEffectiveCB";
+            this.notVeryEffectiveCB.Size = new System.Drawing.Size(68, 19);
+            this.notVeryEffectiveCB.TabIndex = 14;
+            this.notVeryEffectiveCB.Text = "1/2倍";
+            this.notVeryEffectiveCB.UseVisualStyleBackColor = true;
+            this.notVeryEffectiveCB.CheckedChanged += new System.EventHandler(this.notVeryEffectiveCB_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.notVeryEffectiveCB);
+            this.Controls.Add(this.supperEffectiveCB);
+            this.Controls.Add(this.sameTypeCB);
+            this.Controls.Add(this.damageLabel);
+            this.Controls.Add(this.powerLabel);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.defenseLabel);
+            this.Controls.Add(this.attackLabel);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label attackLabel;
+        private System.Windows.Forms.Label defenseLabel;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label powerLabel;
+        private System.Windows.Forms.Label damageLabel;
+        private System.Windows.Forms.CheckBox sameTypeCB;
+        private System.Windows.Forms.CheckBox supperEffectiveCB;
+        private System.Windows.Forms.CheckBox notVeryEffectiveCB;
+    }
+}
+
---
+++ PokemonDamageToolSS/PokemonDamageTool/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PokemonDamageTool
+{
+    public partial class Form1 : Form
+    {
+        private float attackNum;
+
+        private float defenseNum;
+
+        private float powerNum;
+
+        private int minDamage;
+
+        private int maxDamage;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            attackNum = 0;
+
+            defenseNum = 0;
+
+            powerNum = 0;
+
+            minDamage = 0;
+
+            maxDamage = 0;
+        }
+
+
+        //五捨五超入
+        private int RoundedOver(float num)
+        {
+            double d = (double)num;
+            d = d % 1 * 10;
+
+            if (d <= 5)
+            {
+                return (int)num;
+            }
+            
+            return (int)(num + 1);
+        }
+
+        //ダメージ計算
+        private void DamageCalculation()
+        {
+            int damageNum;
+
+            //レベル計算
+            float level = 50 * 2 / 5 + 2;
+            damageNum = (int)level;
+
+            //技威力
+            float pow = damageNum * powerNum * attackNum / defenseNum;
+            damageNum = (int)pow;
+
+            //保障ダメージ
+            float security = damageNum / 50 + 2;
+            damageNum = (int)security;
+
+            //乱数
+            maxDamage = damageNum;
+            minDamage = (int)(damageNum * 0.85f);
+
+            //タイプ一致
+            if (sameTypeCB.Checked)
+            {
+                maxDamage = RoundedOver((maxDamage * 1.5f));
+                minDamage = RoundedOver((minDamage * 1.5f));
+            }
+
+            //タイプ相性
+            //2倍
+            if (supperEffectiveCB.Checked)
+            {
+                maxDamage *= 2;
+                minDamage *= 2;
+            }
+
+            //1/2倍
+            if (notVeryEffectiveCB.Checked)
+            {
+                maxDamage = (int)(maxDamage * 0.5f);
+                minDamage = (int)(minDamage * 0.5f);
+            }
+
+
+            //1より小さくなった場合
+            if (maxDamage < 1)
+                maxDamage = 1;
+
+            if (minDamage < 1)
+                minDamage = 1;
+        }
+
+
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            float i = (float)numericUpDown1.Value;
+
+            attackNum = i;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            float i = (float)numericUpDown2.Value;
+
+            defenseNum = i;
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            float num = (float)numericUpDown3.Value;
+
+            powerNum = num;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DamageCalculation();
+
+            damageLabel.Text = minDamage.ToString() + "～" + maxDamage.ToString();
+
+        }
+
+        private void damageLabel_Click(object sender, EventArgs e)
+        {
+            
+        }
+        
+
+        private void sameTypeCB_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void supperEffectiveCB_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void notVeryEffectiveCB_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
